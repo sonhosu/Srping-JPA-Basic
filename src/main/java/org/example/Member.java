e63780b1d216c51ac919f0fa929fa2b0e41cf271
DESKTOP-JURDD5A\ONLINE-DEV02 <ghtn9398@nate.com>
2023-05-19 15:06:53 +0900

    Member 클래스 생성

---
+++ src/main/java/org/example/Member.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id //PK설정
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
