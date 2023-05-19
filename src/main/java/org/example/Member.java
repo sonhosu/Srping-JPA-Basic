062e929c7ede548b080aa11f121268335ac36001
DESKTOP-JURDD5A\ONLINE-DEV02 <ghtn9398@nate.com>
2023-05-19 16:17:57 +0900

    Member 생성자 추가

--- src/main/java/org/example/Member.java
+++ src/main/java/org/example/Member.java
@@ -10,9 +10,19 @@ public class Member {
     private Long id;
     private String name;
 
+    public Member(){
+
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
+
 
     public void setId(Long id) {
         this.id = id;
